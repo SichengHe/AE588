b1437f7c61633f8b3537647f293dbd1440889b48
Sicheng <hschsc@umich.edu>
2017-02-10 17:12:33 -0500

    Add scipy implementation; combine all method into one uniform form.

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import numpy as np
 import copy
-#import math as math
-#import pylab as pylab
-import matplotlib.pyplot as plt
-import matplotlib.cm as cm
+
+from scipy.optimize import minimize
+
 import BFGS as BFGS
 import trustRegion as TR
 import CG as CG
+
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+""" function """
+
+# problem 1
+def Matyas_class():
+
+	return Matyas, Matyas_grad
+
 
 def Matyas(x):
     
@@ -26,7 +36,143 @@ def Matyas_grad(x):
     
     return np.matrix([[dfdx1],[dfdx2]])
 
+# to make scipy happy
+def Matyas_scipy(x):
+    
+    x1 = x[0]
+    x2 = x[1]
+    
+    return 0.26 * (x1**2 + x2**2) - 0.48 * x1 * x2
 
+
+def Matyas_grad_scipy(x):
+    
+    x1 = x[0]
+    x2 = x[1]
+    
+    dfdx1 = 0.52 * x1 - 0.48 * x2
+    dfdx2 = 0.52 * x2 - 0.48 * x1
+    
+    return np.array([dfdx1, dfdx2])
+
+
+# problem 2
+
+def Rosen_class():
+
+	return Rosen, Rosen_grad
+
+
+def Rosen(x):
+
+	n = x.shape[0]
+
+	J = 0.0
+
+	for i in xrange(n-1):
+
+		x_i = x[i, 0]
+		x_ip1 = x[i+1, 0]
+
+		J += 100.0 * (x_ip1 - x_i**2)**2 + (1.0 - x_i)**2
+
+	return J
+
+def Rosen_grad(x):
+
+	n = x.shape[0]
+
+	dJdx = np.matrix( np.zeros( (n,1) ) )
+
+	for i in xrange(n-1):
+
+		x_i = x[i, 0]
+		x_ip1 = x[i+1, 0]
+
+		dJdxi_loc = 200.0 * (x_ip1 - x_i**2) * (-2.0 * x_i) + \
+					2.0 * (1.0 - x_i) * (-1.0)
+		dJdxip1_loc = 200.0 * (x_ip1 - x_i**2) * 1.0
+
+
+		dJdx[i, 0] += dJdxi_loc
+		dJdx[i+1, 0] += dJdxip1_loc
+
+	return dJdx
+
+# to make scipy happy
+def Rosen_scipy(x):
+
+	n = len(x)
+
+	J = 0.0
+
+	for i in xrange(n-1):
+
+		x_i = x[i]
+		x_ip1 = x[i+1]
+
+		J += 100.0 * (x_ip1 - x_i**2)**2 + (1.0 - x_i)**2
+
+	return J
+
+def Rosen_grad_scipy(x):
+
+	n = len(x)
+
+	dJdx = np.zeros(n)
+
+	for i in xrange(n-1):
+
+		x_i = x[i]
+		x_ip1 = x[i+1]
+
+		dJdxi_loc = 200.0 * (x_ip1 - x_i**2) * (-2.0 * x_i) + \
+					2.0 * (1.0 - x_i) * (-1.0)
+		dJdxip1_loc = 200.0 * (x_ip1 - x_i**2) * 1.0
+
+
+		dJdx[i] += dJdxi_loc
+		dJdx[i+1] += dJdxip1_loc
+
+	return dJdx
+
+
+# x0 = np.matrix(np.zeros((2,1)))
+# x0[0, 0] = 1.0
+# x0[1, 0] = 0.3
+
+x0 = [1.0, 0.3]
+res = minimize(Rosen_scipy, x0, method='BFGS', jac=Rosen_grad_scipy,
+	options={'gtol': 1e-6, 'disp': True})
+print("res",res)
+
+
+# x = np.matrix(np.zeros((2,1)))
+# n = 2
+# x[0, 0] = 1.0
+# x[1, 0] = 2.0
+
+# J = Rosen(x)
+
+# x_perturb_1 = copy.deepcopy(x)
+# x_perturb_1[0, 0] += 1e-8
+# J_perturb_1 = Rosen(x_perturb_1)
+
+# x_perturb_2 = copy.deepcopy(x)
+# x_perturb_2[1, 0] += 1e-8
+# J_perturb_2 = Rosen(x_perturb_2)
+
+# dJ_finte = np.matrix(np.zeros((2,1)))
+# dJ_finte[0, 0] = (J_perturb_1 - J)/ 1e-8
+# dJ_finte[1, 0] = (J_perturb_2 - J)/ 1e-8
+
+# print("dJ_finte", dJ_finte, "Rosen_grad", Rosen_grad(x)) 
+
+
+
+
+
+""" post process """
 def traj(x):
 
 	"2D trajectory of optimization"
@@ -77,72 +223,82 @@ def postProcess2D(x_low, x_up, Nx, y_low, y_up, Ny, f):
 
 
 
-V0 = np.matrix(np.eye(2))         
-x0 = np.matrix(np.zeros((2,1)))
-x0[0, 0] = 1.0
-x0[1, 0] = 0.3
-dim_N = 2
-
-xmin = -1.0
-xmax = 1.0
-ymin = -1.0
-ymax = 1.0
-Nx = 100
-Ny = 100
-            
-p1 = BFGS.BFGS(Matyas, Matyas_grad, x0, V0, dim_N, 1)
-x_list_LS, n_iter_list_LS, log_g_norm_list_LS = p1.optimize(10**-12)
-
-x1_LS, x2_LS = traj(x_list_LS)     
-      
-        
-        
-        
-        
-        
-B_0 = np.matrix(np.eye(2))     
-epsilon = 1e-12
-        
-Delta_0 = 1.0
-Delta_max = 10.0
-x_list_TR, n_iter_list_TR, log_g_norm_list_TR = TR.trustRegion(Delta_0, Delta_max, epsilon, x0, B_0, Matyas, Matyas_grad, 1)     
-        
-
-x1_TR, x2_TR = traj(x_list_TR)    
-
-  
-
-        
-   
-    
-        
-p3 = CG.CG(Matyas, Matyas_grad, x0, 1)   
-x_list_CG, n_iter_list_CG, log_g_norm_list_CG = p3.optimize(epsilon)
-
-x1_CG, x2_CG = traj(x_list_CG)   
-        
-        
-
-if (1 == 1):        
-        
-	fig = postProcess2D(xmin, xmax, Nx, ymin, ymax, Ny, Matyas)  
-
-	plt.plot(x1_LS, x2_LS, '-yo')
-	plt.plot(x1_TR, x2_TR, '-rx') 
-	plt.plot(x1_CG, x2_CG, '-g*')  
-
-if (1 == 1):
-
-	plt.figure()
-
-	plt.plot(n_iter_list_LS, log_g_norm_list_LS,'-yo')
-	plt.plot(n_iter_list_TR, log_g_norm_list_TR,'-rx')
-	plt.plot(n_iter_list_CG, log_g_norm_list_CG,'-g*')
 
 
 
-plt.show()           
-           
-        
-        
+
+
+
+
+
+def uncon(func, x0, max_iter, tol):
+
+	method = "BFGS_LS"
+	mode = 0  # solve mode
+	#mode = 1 # analysis mode
+
+	# get the obj function and gradient
+
+	f, g = func()
+
+	n = x0.shape[0]	
+
+	if (method == "BFGS_LS"):
+
+		V0 = np.matrix(np.eye(n))
+
+		p = BFGS.BFGS(f, g, x0, V0, n, mode)
+
+		if (mode == 0):
+
+			x, J = p.optimize(tol)
+
+		elif (mode == 1):
+
+			x_list, n_iter_list, log_g_norm_list = p.optimize(tol)
+
+	elif (method == "BFGS_TR"):
+
+		B_0 = np.matrix(np.eye(n))
+
+		Delta_0 = 1.0
+		Delta_max = 10.0
+
+		if (mode == 0):
+
+			x, J = TR.trustRegion(Delta_0, Delta_max, tol, \
+				x0, B_0, f, g, mode) 
+
+		elif (mode == 1):
+
+			x_list, n_iter_list, log_g_norm_list = TR.trustRegion(Delta_0, Delta_max, tol, \
+				x0, B_0, f, g, mode) 
+
+
+	elif (method == "CG"):
+
+		p = CG.CG(f, g, x0, mode)   
+		if (mode == 0):
+
+			x, J = p.optimize(tol)
+
+		elif (mode == 1):
+
+			x_list, n_iter_list, log_g_norm_list = p.optimize(tol)
+
+
+
+	if (mode == 0):
+
+		return x, J
+
+	elif (mode == 1):
+
+		return x_list, n_iter_list, log_g_norm_list
+
+
+
+
+
+
 
